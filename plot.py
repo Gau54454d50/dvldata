31462d8e62fbbcaac5baae82bab379b1b3ff8e90
Gau54454d50 <universedo@gmail.com>
2023-03-23 14:59:04 +0530

    i/\/a\/m\//a\//n\/o\/t\/h\/i\/n/\/g\//n\/o\/b\/o\/d\/y

--- plot.py
+++ plot.py
@@ -1,16 +1,48 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
+import os
 
-pd.set_option('display.width', 5000) 
-pd.set_option('display.max_columns', 60)
-plt.rcParams['figure.figsize'] = (15, 5)
+class Time :
+    def __init__(self,seconds,minutes,hours) :
+        self.seconds = float(seconds)
+        self.minutes = int(minutes)
+        self.hours = int(hours)
+    def findtime(self):
+        return float(self.hours*60*60+self.minutes*60)+self.seconds
 northing = 496061.20
 easting = 2458748.38
-df = pd.read_csv('Dwarka_16_DVL.csv')
+df = pd.read_csv('2023-01-16 12-18-52 vehicle1.csv')
+df1 = pd.read_csv('Dwarka.csv')
+nan = []
+print("TOF = ",type(df1["ts"][len(df1["ts"])-2]),type(pd.np.nan))
+for r in range(0,len(df1["ts"])-1,14):
+    print(np.isnan(df1["ts"][r]))
+    if isnan(df1["ts"][r]):
+        nan.append(r)
+print(len(nan))
 x=[]
 y=[]
 i=0
+diff=[]
+t=[]
 
+for time in df["Timestamp"]:
+    time = time.split(" ")
+    time = time[1].split(":")
+    t.append(Time(time[2],time[1],time[0]))
+    #print(time[2],"type of =",type(time))
+t.index(t[0])
+for time in t:
+    if t.index(time) == 0:
+        print("starting ...")
+    elif t.index(time) == len(t)-1:
+        print("stopping ...")
+    else :
+        diff.append(time.findtime()-t[t.index(time)-1].findtime())
+
+print(len(diff))
+'''
 for (vx,vy,dt) in (df["vx"],df["vy"],df["time"]) :
     X = vx * dt + x[i]
     x.append(X)
@@ -20,3 +52,4 @@ for (vx,vy,dt) in (df["vx"],df["vy"],df["time"]) :
     s = 0
 #df.plot(figsize=(15, 10))
 #plt.show()
+'''
